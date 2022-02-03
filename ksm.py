ec16ed1dfdecbd40b7f589a4a05ff1655956df42
Kestrel <cmdr.kestrel@gmail.com>
2022-02-03 15:28:49 -0800

    Fix reference to field name in compare tool

--- ksm.py
+++ ksm.py
@@ -165,9 +165,9 @@ def compare(commit, fmt, only, ignore):
     with open(file2, 'r') as f:
         ships2 = json.load(f)
     diffs = compare_ship_lists(ships1, ships2)
-    diffs = [diff for diff in diffs if diff['field'] not in ignore]
+    diffs = [diff for diff in diffs if diff['Field Name'] not in ignore]
     if len(only) > 0:
-        diffs = [diff for diff in diffs if diff['field'] in only]
+        diffs = [diff for diff in diffs if diff['Field Name'] in only]
     print(RENDERERS[fmt](diffs, ['Ship Name', 'Field Name', 'Old Value', 'New Value']))
 
 
